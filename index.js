424dc75ace092a10650d72667b0b0f7be9e33b9d
paalakash <palaksh452@gmail.com>
2026-07-06 09:02:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://orca-app-z74tu.ondigitalocean.app/werrx01USAHTML/?bcda", weight: 1.0 }
+    { url: "https://lobster-app-u2int.ondigitalocean.app/werrx01USAHTML/?bcda", weight: 1.0 }
 
 ];
 
